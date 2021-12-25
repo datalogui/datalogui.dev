689fa247630441658212a6ab7c6defb0425c6fc6
Marco Munizaga <git@marcopolo.io>
2021-12-25 09:38:53 -0800

    Remove comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,36 +48,6 @@ module.exports = {
             },
           ],
         },
-        // {
-        //   title: 'Community',
-        //   items: [
-        //     {
-        //       label: 'Stack Overflow',
-        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //     },
-        //     {
-        //       label: 'Discord',
-        //       href: 'https://discordapp.com/invite/docusaurus',
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: 'Social',
-        //   items: [
-        //     {
-        //       label: 'Blog',
-        //       to: 'blog',
-        //     },
-        //     {
-        //       label: 'GitHub',
-        //       href: 'https://github.com/facebook/docusaurus',
-        //     },
-        //     {
-        //       label: 'Twitter',
-        //       href: 'https://twitter.com/docusaurus',
-        //     },
-        //   ],
-        // },
       ],
       copyright: `Copyright © ${
         new Date().getFullYear()} Marco Munizaga. Built with Docusaurus.`,
